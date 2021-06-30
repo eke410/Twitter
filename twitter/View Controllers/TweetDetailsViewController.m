fa589338cb557941004a82e1f2f6f5a3c9f8b228
eke410 <elizabethke100@gmail.com>
2021-06-30 18:24:18 -0400

    added (un)retweet and (un)favorite functionality to tweet details page

--- twitter/View Controllers/TweetDetailsViewController.m
+++ twitter/View Controllers/TweetDetailsViewController.m
@@ -8,6 +8,7 @@
 
 #import "TweetDetailsViewController.h"
 #import "Tweet.h"
+#import "APIManager.h"
 #import "DateTools.h"
 #import "UIImageView+AFNetworking.h"
 
@@ -54,6 +55,59 @@
     self.tweet.retweeted ? [self.retweetButton setImage:[UIImage imageNamed:@"retweet-icon-green"] forState:UIControlStateNormal] : [self.retweetButton setImage:[UIImage imageNamed:@"retweet-icon"] forState:UIControlStateNormal];
 }
 
+- (IBAction)didTapRetweet:(id)sender {
+    if (!self.tweet.retweeted) {
+        self.tweet.retweeted = YES;
+        self.tweet.retweetCount += 1;
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else {
+        self.tweet.retweeted = NO;
+        self.tweet.retweetCount -= 1;
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    [self refreshData];
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    if (!self.tweet.favorited) {
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else {
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error unfavoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    [self refreshData];
+}
 
 
 
